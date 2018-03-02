2c4d96aa69d145ea403c30ddeed4c48f713c43e9
Tim <tim.huang61@gmail.com>
2018-03-02 14:56:22 +0800

    add no budget test case

---
+++ TddBudget/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+
+namespace TddBudget
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NoBudgets()
+        {
+            IBudgetRepository budgetRepository = Substitute.For<IBudgetRepository>();
+            budgetRepository.GetAll().Returns(new List<Budget>());
+            var budgetService = new BudgetService(budgetRepository);
+
+            Assert.AreEqual(0, budgetService.CalBudgets(new DateTime(2018, 4, 1), new DateTime(2018, 4, 5)));
+        }
+    }
+}
