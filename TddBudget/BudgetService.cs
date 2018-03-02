01ea11769d294d372a718e8ce588066a3e096274
Tim <tim.huang61@gmail.com>
2018-03-02 14:43:21 +0800

    add budget service and IRepository

---
+++ TddBudget/BudgetService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddBudget
+{
+    public class BudgetService
+    {
+        private readonly IBudgetRepository _budgetRepository;
+
+        public BudgetService(IBudgetRepository budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+        }
+
+        public decimal CalBudgets(DateTime startDate, DateTime endDate)
+        {
+            return 0;
+        }
+    }
+}
